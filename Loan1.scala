af2c0af0f589f6c249bcaed6db4b68adece3d6af
preneetho <preneetho@gmail.com>
2016-01-26 11:53:38 -0800

    Working

--- Loan1.scala
+++ Loan1.scala
@@ -3,31 +3,28 @@ import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 
 object Loan1{
+
+	//Program to fetch records with Grade == E
+	
+        def removeQuotes(str: String) : String = {
+	        val newString = str.replaceAll("\"","") 
+	        return newString
+        }
+        
+        
         def main(args: Array[String]): Unit = {
           
             val conf = new SparkConf().setAppName("MyLoan1")
             val sc = new SparkContext(conf)
-          
             // Read the CSV file
             val csv =sc.textFile("hdfs:/preneeth_hdfs/LoanData.csv")
-          
             // split / clean data
 	    val data = csv.map(line => line.split(",").map(_.trim).map(x => removeQuotes(x)))
-	   
-	   //eliminate junk data
-          val goodData = data.collect { case l if (l.length > 8) => (l)}
-	   
-	   //eliminate junk data
-           val filterData = goodData.filter(x => x.map(y => y.split(",")(8) == "E"))
-           
-           filterData.foreach(println)
-	   
+	    //eliminate junk data
+            val goodData = data.collect { case l if (l.length > 8) => (l)}
+	    val header = List ("COL1","COL2","COL3","COL4","COL5","COL6","COL7","COL8","COL9")
+	    val maps = goodData.map(splits => header.zip(splits).toMap)
+            val result = maps.filter(x => x("COL9") == "E")
+            result.foreach(println)
         }
-        
-        def removeQuotes(str: String) : String = {
-        val newString = str.replaceAll("\"","") 
-        return newString
-        }
-        
-        
 }
