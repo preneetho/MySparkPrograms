6754119b28dd394c569cbfed7f731ea226a5f26b
preneetho <preneetho@gmail.com>
2016-01-25 19:53:24 -0800

    Added Commments

--- YouTube.scala
+++ YouTube.scala
@@ -4,25 +4,29 @@ import org.apache.spark.SparkConf
 
 object YouTube{
         def main(args: Array[String]): Unit = {
-          // Read the CSV file
+          
           val conf = new SparkConf().setAppName("MyTube")
           val sc = new SparkContext(conf)
+          // Read the CSV file
           val csv =sc.textFile("hdfs:/preneeth_hdfs/youtubedataNew.txt")
-          // split / clean data
-          //val data = csv.map(line => line.split("\t")(3)).map(e=>e.trim)
           
+          //split each line item into a map
           val data = csv.map(line => line.split("\t").map(e=>e.trim))
           
+          //COL4 is not present in few records, so eliminate it and form a tuple with (category, 1)
           val goodData = data.collect { case l if (l.length > 3) => (l(3), 1)}
           
+          //sum all categories
           val countRDD = goodData.reduceByKey(_ + _)
           
+          //Swap the tuple to sort on key.
           val sortRDD = countRDD.map (x => x.swap).sortByKey(false)
           
+          //Sort (desending) and get top 5
           val top5 = sortRDD.sortBy(_._1, false).take(5)
-                    
-          sortRDD.foreach(println)
+          
+          //Print
+          top5.foreach(println)
           sc.stop
-          //Modified files on Server
         }
 }
